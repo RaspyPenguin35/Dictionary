96174c68ecd0f2a68810a387a7ebc03545cecd0f
RaspyPenguin35 <nabanita.biswas@gmail.com>
2024-11-02 09:47:22 -0400

    dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,3 +1,21 @@
 Aunwita_Dictionary={"name":"Aunwita","age":10,"city":"Plainsboro NJ"}
 print (Aunwita_Dictionary)
 print (Aunwita_Dictionary["name"])
+Aunwita_List=["Aunwita",10,"Plainsboro NJ"]
+print(Aunwita_List[0])
+print(Aunwita_List[1])
+print(Aunwita_List[2])
+print(Aunwita_Dictionary.keys())
+print(Aunwita_Dictionary.values())
+if "name" in Aunwita_Dictionary:
+    print(Aunwita_Dictionary["name"])
+Aunwita_Dictionary["phonenumber"]=7657144361
+print(Aunwita_Dictionary["phonenumber"])
+print(Aunwita_Dictionary)
+Aunwita_Dictionary['age']=11
+print(Aunwita_Dictionary)
+del Aunwita_Dictionary["phonenumber"]
+print(Aunwita_Dictionary)
+even_numbers={"num":[0,2,4,6,8,10,12,14,16,18,20]}
+print(even_numbers)
+print(Aunwita_Dictionary)
